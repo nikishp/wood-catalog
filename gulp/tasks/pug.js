e29c435d467e12434cf1d4fdcbbbaf65d004c7e5
Niki Simple <shpakovskiynl@gmail.com>
2018-10-30 01:54:24 +0200

    new

---
+++ gulp/tasks/pug.js
@@ -0,0 +1,45 @@
+module.exports = function() {
+  $.gulp.task('pug:dev', ()=>  {
+    return $.gulp.src('dev/pug/pages/*.pug')
+    //      .pipe($.changed('build', {extension: '.html'}))// работает очень быстро, но перезагружает страницу если изменялись страницы, на модули не работает
+    //      .pipe($.changed('dev/pug', {extension: '.pug'}))// работает чуть медленнее, но следит за всеми файлами
+    //      .pipe($.jadeInheritance({basedir: 'dev/pug/pages'}))
+      .pipe($.gp.pug({
+      locals : {
+        nav: JSON.parse($.fs.readFileSync('data/navigation.json', 'utf8')),
+        content: JSON.parse($.fs.readFileSync('data/content.json', 'utf8')),
+      },
+      pretty: true
+    }))
+      .on('error', $.gp.notify.onError(function(error) {
+      return {
+        title: 'Pug',
+        message: error.message
+      };
+    }))
+      .pipe($.gulp.dest('build/'))
+    //      .on('end', $.browserSync.reload);
+      .pipe($.browserSync.reload({
+      stream: true
+    }))
+  });
+
+  $.gulp.task('pug:build', ()=>  {
+    return $.gulp.src('dev/pug/pages/*.pug')
+      .pipe($.gp.pug({
+      locals : {
+        nav: JSON.parse($.fs.readFileSync('data/navigation.json', 'utf8')),
+        content: JSON.parse($.fs.readFileSync('data/content.json', 'utf8')),
+      },
+      pretty: true
+    }))
+      .on('error', $.gp.notify.onError(function(error) {
+      return {
+        title: 'Pug',
+        message: error.message
+      };
+    }))
+      .pipe($.gp.removeCode({ production: true }))
+      .pipe($.gulp.dest('build/'));
+  });
+};
